f2fba7d0479914a874ace5a7cedc6270bccec2bb
NNVG <g696607013@gmail.com>
2023-07-05 17:00:27 +0100

    Creat Components

--- src/data/chartdata.tsx
+++ src/data/chartdata.tsx
@@ -1,28 +1,21 @@
-const DATA = [
-  {
-    "id": 1,
-    "name": 'Viany',
-    "photos": require('./../../assets/images.jpeg'),
-    "lastMessage": 'How are ypou doing?',
-    "time": '10:10 AM',
-    "totalUnread": 1
-  },
-  {
-    "id": 2,
-    "name": 'Charles',
-    "photos": require('./../../assets/image2.jpg'),
-    "lastMessage": 'Hello niga!!!!!',
-    "time": '11:10 AM',
-    "totalUnread": 2
-  },
-  {
-    "id": 3,
-    "name": 'Alan',
-    "photos": require('./../../assets/image3.jpg'),
-    "lastMessage": 'Hello!!!',
-    "time": '10:00 AM',
-    "totalUnread": 3
-  }
-]
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
 
-export default DATA
+const chartdata = () => {
+  return (
+    <View style={styles.container}>
+      <Text>chartdata</Text>
+    </View>
+  )
+}
+
+export default chartdata
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
