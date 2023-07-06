594106349708b360b1ddb96a0e31a766b27f7055
charles <ketchasso72@gmail.com>
2023-07-06 17:01:43 +0100

    build login page

--- App.tsx
+++ App.tsx
@@ -1,52 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import { LogBox, StyleSheet, Text, View } from "react-native";
+import { LogBox, Text } from "react-native";
 import { useAssets } from "expo-asset";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./config/firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignIn from "./screen/SignIn";
+import ContextWrapper from "./context/ContextWrapper";
 
 LogBox.ignoreLogs([
   "setting a timer",
   "AsyncStorage has been extracted  from react-native core and will be removed in feature release",
 ]);
 
+const Stack = createStackNavigator();
+
 function App() {
   const [currentUser, setCurrentUser] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<Boolean>(true);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setLoading(false);
       if (user) {
         setCurrentUser(user);
       }
     });
-    return unsubscribe();
-  }, []);
+    return () => unsubscribe();
+  }, []); */
 
-  if (loading) {
-    return<Text>Loading...</Text>
-  }
+  /*   if (!loading) {
+    return <Text>Loading...</Text>;
+  } */
 
   return (
-    <View style={styles.container}>
-      <Text>{JSON.stringify(currentUser)}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {!currentUser ? (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="signIn" component={SignIn} />
+        </Stack.Navigator>
+      ) : (
+        <Text>Hello</Text>
+      )}
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#012",
-  },
-});
-
-function main() {
+function Main() {
   const [assets] = useAssets([
     require("./assets/user-icon.png"),
     require("./assets/welcome-img.png"),
@@ -54,7 +54,10 @@ function main() {
   if (!assets) {
     return <Text>Loading...</Text>;
   }
-  return <App />;
+  return (
+    <ContextWrapper>
+      <App />
+    </ContextWrapper>
+  );
 }
-
-export default main();
+export default Main;
---
+++ screen/SignIn.tsx
@@ -0,0 +1,76 @@
+import { View, Text, Image, TextInput, Button } from "react-native";
+import React, { useContext, useState } from "react";
+import Context from "../context/Context";
+import { signIn, signUp } from "../config/firebase";
+
+export default function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [mode, setMode] = useState<any>("signUp");
+  const {
+    theme: { colors },
+  } = useContext(Context);
+/* 
+  async function handlePress() {
+    if (mode === "signUp") {
+      await signUp(email, password);
+    }
+    if (mode === "signIn") {
+      await signIn(email, password);
+    }
+  } */
+
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: colors.white,
+      }}
+    >
+      <Text
+        style={{ color: colors.foreground, fontSize: 24, marginBottom: 20 }}
+      >
+        Welcome To Whatsapp
+      </Text>
+      <Image
+        source={require("../assets/welcome-img.png")}
+        style={{ width: 180, height: 180 }}
+        resizeMode="cover"
+      />
+      <View style={{ marginTop: 20 }}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 2,
+            width: 200,
+          }}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 2,
+            width: 200,
+            marginTop: 20,
+          }}
+        />
+        <View style={{ marginTop: 20 }}>
+          <Button
+            title= "si"/* {mode === "SignUp" ? "sign Up" : "Login in"} */
+            color={colors.secondary}
+            disabled={!password || !email}
+/*             onPress={handlePress} */
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
