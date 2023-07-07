74d566c913234975362629b3babafccc8a1e5754
NNVG <g696607013@gmail.com>
2023-07-07 16:09:26 +0100

    Create  status data

---
+++ src/screens/Status.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Status = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Status</Text>
+    </View>
+  )
+}
+
+export default Status
