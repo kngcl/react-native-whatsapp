778c1d26adeaa3357a612d356a5fa2c2d3f6d16b
charles <ketchasso72@gmail.com>
2023-07-09 20:02:30 +0100

    profile style

--- screen/Profile.tsx
+++ screen/Profile.tsx
@@ -1,10 +1,18 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  Button,
+} from "react-native";
 import React, { useContext, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
 import GlobalContext from "../context/Context";
 import Context from "../context/Context";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { pickImage } from "../utils";
 
 export default function Profile() {
   const [displayName, setDisplayName] = useState<string>("");
@@ -13,6 +21,16 @@ export default function Profile() {
   const {
     theme: { colors },
   } = useContext(GlobalContext);
+
+  async function handlePress() {}
+
+  async function handleImagePicker() {
+    const result = await pickImage();
+
+    if (!result.cancelled) {
+      setSelectedImage(result);
+    }
+  }
 
   return (
     <React.Fragment>
@@ -29,9 +47,50 @@ export default function Profile() {
         <Text style={{ fontSize: 14, color: colors.text, marginTop: 20 }}>
           Hi bro please enter your image and displayName
         </Text>
-        <TouchableOpacity>
-          {!selectedImage ? <MaterialCommunityIcons /> : /*  <Image /> */ "f"}
+        <TouchableOpacity
+          onPress={handleImagePicker}
+          style={{
+            marginTop: 20,
+            borderRadius: 120,
+            width: 120,
+            height: 120,
+            backgroundColor: colors.background,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {!selectedImage ? (
+            <MaterialCommunityIcons
+              name="camera-plus"
+              color={colors.iconGray}
+              size={45}
+            />
+          ) : (
+            <Image
+              source={{ uri: selectedImage }}
+              style={{ width: "100%", borderRadius: 120 }}
+            />
+          )}
         </TouchableOpacity>
+        <TextInput
+          placeholder="Enter your Name"
+          value={displayName}
+          onChangeText={setDisplayName}
+          style={{
+            borderBottomColor: colors.primary,
+            marginTop: 40,
+            borderBottomWidth: 2,
+            width: "100%",
+          }}
+        />
+        <View style={{ marginTop: "auto", width: 80 }}>
+          <Button
+            title="Next"
+            color={colors.secondary}
+            onPress={handlePress}
+            disabled={!displayName}
+          />
+        </View>
       </View>
     </React.Fragment>
   );
--- utils.js
+++ utils.js
@@ -1,26 +1,38 @@
-const palette = {
-    tealGreen:"#128c7e",
-    tealGreenDark: "#075e54",
-    green:"#25d366",
-    lime: "#dcf8c6",
-    skyblue:"#34b7f1",
-    smokeWhite: "#ece5dd",
-    white:"white",
-    gray:"#3C3C3C",
-    lightGray:"#757575",
-    iconGray:"#717171"
+import * as ImagePicker from "expo-image-picker";
+
+export async function pickImage() {
+  let result = await ImagePicker.launchCameraAsync();
+  return result;
 }
 
-export const theme = {
-    colors:{
-        background : palette.smokeWhite,
-        foreground : palette.tealGreenDark,
-        primary : palette.tealGreen,
-        tertiary : palette.lime,
-        secondary : palette.green,
-        white : palette.white,
-        text : palette.gray,
-        secondaryText : palette.lightGray,
-        iconGray : palette.iconGray
-    },
+export async function askForPermission() {
+  const { status } = await ImagePicker.requestCameraPermission();
+  return status;
 }
+
+const palette = {
+  tealGreen: "#128c7e",
+  tealGreenDark: "#075e54",
+  green: "#25d366",
+  lime: "#dcf8c6",
+  skyblue: "#34b7f1",
+  smokeWhite: "#ece5dd",
+  white: "white",
+  gray: "#3C3C3C",
+  lightGray: "#757575",
+  iconGray: "#717171",
+};
+
+export const theme = {
+  colors: {
+    background: palette.smokeWhite,
+    foreground: palette.tealGreenDark,
+    primary: palette.tealGreen,
+    tertiary: palette.lime,
+    secondary: palette.green,
+    white: palette.white,
+    text: palette.gray,
+    secondaryText: palette.lightGray,
+    iconGray: palette.iconGray,
+  },
+};
