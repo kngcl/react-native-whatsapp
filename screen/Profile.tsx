51a2417aa2e5a6d54ba03bd405af3501cfed6a60
charles <ketchasso72@gmail.com>
2023-07-10 14:43:56 +0100

    PROFILE screen done

--- screen/Profile.tsx
+++ screen/Profile.tsx
@@ -6,29 +6,78 @@ import {
   TextInput,
   Button,
 } from "react-native";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
 import GlobalContext from "../context/Context";
 import Context from "../context/Context";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { pickImage } from "../utils";
+import { pickImage, askForPermission, uploadImage } from "../utils";
+import { auth, db } from "../config/firebase";
+import { updateProfile } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Profile() {
   const [displayName, setDisplayName] = useState<string>("");
   const [selectedImage, setSelectedImage] = useState<any>(null);
+  const [permission, setPermission] = useState<any>(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    async () => {
+      const status = await askForPermission();
+      setPermission(status);
+      permissions();
+    };
+  }, []);
 
   const {
     theme: { colors },
   } = useContext(GlobalContext);
 
-  async function handlePress() {}
+  async function handlePress() {
+    const user = auth.currentUser;
+    let photoURL;
+    console.log(user);
+    if (selectedImage) {
+      const { url } = await uploadImage(
+        selectedImage,
+        `images/${user?.uid}`,
+        "profilePicture"
+      );
+      photoURL = url;
+    }
+    const userData = {
+      displayName,
+      email: user?.email,
+    };
+    if (photoURL) {
+      userData.photoURL = photoURL;
+    }
+    await Promise.all([
+      updateProfile(user, userData),
+      setDoc(doc(db, "users", user.uid), { ...userData, uid: user?.uid }),
+    ]);
+        navigation.navigate("home");
+  }
 
   async function handleImagePicker() {
     const result = await pickImage();
 
-    if (!result.cancelled) {
-      setSelectedImage(result);
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result.assets[0].uri);
+    }
+  }
+
+  function permissions() {
+    if (!permission) {
+      return <Text>Loading...</Text>;
+    }
+
+    if (permission != "granted") {
+      return <Text>You need to give access</Text>;
     }
   }
 
@@ -41,6 +90,7 @@ export default function Profile() {
           justifyContent: "center",
           flex: 1,
           paddingTop: Constants.statusBarHeight + 20,
+          padding: 20,
         }}
       >
         <Text style={{ fontSize: 22, color: colors.foreground }}>Profile</Text>
@@ -50,7 +100,7 @@ export default function Profile() {
         <TouchableOpacity
           onPress={handleImagePicker}
           style={{
-            marginTop: 20,
+            marginTop: 30,
             borderRadius: 120,
             width: 120,
             height: 120,
--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
 import * as ImagePicker from "expo-image-picker";
+import { storage } from "./config/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { nanoid } from "nanoid";
+import "react-native-get-random-values";
 
 export async function pickImage() {
   let result = await ImagePicker.launchCameraAsync();
@@ -8,6 +12,35 @@ export async function pickImage() {
 export async function askForPermission() {
   const { status } = await ImagePicker.requestCameraPermissionsAsync();
   return status;
+}
+
+export async function uploadImage(uri, path, fname) {
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      console.log(e);
+      reject(new TypeError("Network request failed"));
+    };
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
+    xhr.send(null);
+  });
+
+  const fileName = fname || nanoid();
+  const imageRef = ref(storage, `${path}/${fileName}.jpeg`);
+
+  const snapshot = await uploadBytes(imageRef, blob, {
+    contentType: "image/jpeg",
+  });
+
+  blob.close();
+
+  const url = await getDownloadURL(snapshot.ref);
+
+  return { url, fileName };
 }
 
 const palette = {
