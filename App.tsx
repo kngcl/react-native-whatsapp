c9555aaa24ebba0ec1f07d6e5fedf5c574ea2b62
charles <ketchasso72@gmail.com>
2023-07-11 14:37:38 +0100

    merge code

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { LogBox, Text } from "react-native";
 import { useAssets } from "expo-asset";
 import React, { useContext, useEffect, useState } from "react";
@@ -9,12 +10,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "./screen/SignIn";
 import ContextWrapper from "./context/ContextWrapper";
 import Profile from "./screen/Profile";
+=======
+import { StatusBar } from "expo-status-bar";
+import { LogBox, StyleSheet, Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+>>>>>>> c7b7989 (setup email and password)
 
 LogBox.ignoreLogs([
   "setting a timer",
   "AsyncStorage has been extracted  from react-native core and will be removed in feature release",
 ]);
 
+<<<<<<< HEAD
 const Stack = createStackNavigator();
 
 function App() {
@@ -23,6 +33,11 @@ function App() {
   const {
     theme: { colors },
   } = useContext(Context);
+=======
+function App() {
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+>>>>>>> c7b7989 (setup email and password)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -31,6 +46,7 @@ function App() {
         setCurrentUser(user);
       }
     });
+<<<<<<< HEAD
     return () => unsubscribe();
   }, []);
 
@@ -78,10 +94,39 @@ function Home() {
 }
 
 function Main() {
+=======
+    return unsubscribe();
+  }, []);
+
+  if (loading) {
+    return<Text>Loading...</Text>
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{JSON.stringify(currentUser)}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#012",
+  },
+});
+
+function main() {
+>>>>>>> c7b7989 (setup email and password)
   const [assets] = useAssets([
     require("./assets/user-icon.png"),
     require("./assets/welcome-img.png"),
   ]);
+<<<<<<< HEAD
 
   if (!assets) {
     return <Text>Loading...</Text>;
@@ -93,3 +138,12 @@ function Main() {
   );
 }
 export default Main;
+=======
+  if (!assets) {
+    return <Text>Loading...</Text>;
+  }
+  return <App />;
+}
+
+export default main();
+>>>>>>> c7b7989 (setup email and password)
